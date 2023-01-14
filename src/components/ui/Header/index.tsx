10a973fd5d71361d1628689df1d793e3498169d7
ronrix <ronrixl5@gmail.com>
2023-01-14 12:17:41 +0800

    Fix folder strucure

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
 
-import Auth from "../auth/Auth";
+import Auth from "../../auth/Auth";
 
 export default function Header() {
   const [showLoginForm, setShowLoginForm] = useState<boolean>(false);
