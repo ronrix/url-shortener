9be7eaf1c37aacc06e991f9957938ef13aeef7dc
ronrix <ronrixl5@gmail.com>
2023-01-20 20:02:36 +0800

    Fix issue for getting the value of the data from data[0] to data

--- server/modules/functions/generateAccessToken.js
+++ server/modules/functions/generateAccessToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const secret = "SECRET";
 
 function generateAccessToken(data, options) {
-   return jwt.sign(data[0], secret, options);
+   return jwt.sign(data, secret, options);
 }
 
 module.exports = generateAccessToken;
