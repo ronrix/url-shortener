fdb6412cb01d0dd32bf3b348d31a96ed6a044c47
ronrix <ronrixl5@gmail.com>
2023-01-21 01:05:33 +0800

    Change method
    I change the _query method to _exists to return true/false if data exists

--- server/models/auth.model.js
+++ server/models/auth.model.js
@@ -28,7 +28,14 @@ class AuthModel extends Connection {
 
 					const user = result[0];
 					if(error) {
-						reject({ msg: "User not found", status: 404 });
+						reject({ msg: "Something went wrong", status: 409 });
+						return;
+					}
+
+					// no result has found
+					if(!result.length) {
+						reject({ msg: "User not found!", status: 404 });
+						return;
 					}
 
 					const valid = await bcryt.compare(fields.password, user.password);
@@ -36,7 +43,7 @@ class AuthModel extends Connection {
 						resolve({ id: user.id, username: user.username });
 					}
 
-					reject({ msg: "Username or password is incorrect", status: 400 });
+					reject({ msg: "Username or password is incorrect", status: 401 });
 				});
 		}) 
 	}
@@ -45,21 +52,28 @@ class AuthModel extends Connection {
 
 		// add form validation here
 		if(this.FormValidation.is_empty(fields)) {
-			throw {msg: "Please fill up the fields", status: 400};
+			throw {msg: "Please fill up the fields", status: 409};
 		}
 		if(!this.FormValidation.is_password_valid(fields.password)) {
-			throw {msg: "Please make sure your password is 8 characters long", status: 400};
+			throw {msg: "Please make sure your password is 8 characters long", status: 409};
 		}
 
 		const hashed_password = await bcryt.hash(fields.password, salt);
 
+		// check if username is already used
+		const user_found = await this._exists("SELECT * FROM users WHERE users.username = ? LIMIT 1", [fields.username]);
+		if(user_found) {
+			throw { msg: "User already exists", status: 409 };
+		}
+
+		// create new user if user is valid
 		return new Promise(async (resolve, reject) => {
 			await this.connection.execute(`
 				INSERT INTO users(username, password) 
 				VALUES(?, ?)`, 
 				[fields.username, hashed_password], function(err, result, _) {
 					if(err) {
-						reject({msg: err, status: 400});
+						reject({msg: err, status: 500});
 					}
 					if(result.affectedRows) {
 						resolve({ id: result.insertId, username: fields.username });
