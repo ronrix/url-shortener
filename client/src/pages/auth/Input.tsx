d204d63b070759fec3e8d25b023642b808d9f653
ronrix <ronrixl5@gmail.com>
2023-01-21 23:48:27 +0800

    Modify the useForm
    I added the yup to handle the useForm validation. So i removed some of the props since yup resolver handles it already. So users can see the realtime error while their typing

--- client/src/pages/auth/Input.tsx
+++ client/src/pages/auth/Input.tsx
@@ -1,35 +1,41 @@
 import React from "react";
-import { FieldError, FieldErrorsImpl, Merge, useForm } from "react-hook-form";
+import {
+  FieldValues,
+  UseFormRegister,
+  FieldError,
+  FieldErrorsImpl,
+  Merge,
+} from "react-hook-form";
 
 type Props = {
   name: string;
-  required: {};
   placeholder: string;
-  errorMsg: string;
-  error: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined;
   type?: string;
-  register: any;
+  register: UseFormRegister<FieldValues>;
+  error:
+    | string
+    | FieldError
+    | Merge<FieldError, FieldErrorsImpl<any>>
+    | undefined;
 };
 
 export default function Input({
   name,
-  required,
   placeholder,
-  errorMsg,
-  error,
   type,
   register,
+  error,
 }: Props) {
   return (
     <>
       <input
-        {...register(name, required)}
+        {...register(name)}
         className="border p-3 my-2 bg-slate-100 focus:bg-white rounded-lg"
         placeholder={placeholder}
         type={type}
       />
       {error && (
-        <p className="text-red-600 text-left text-xs m-2 mt-0">{errorMsg}</p>
+        <p className="text-red-600 text-left text-xs m-2 mt-0">{`${error}`}</p>
       )}
     </>
   );
