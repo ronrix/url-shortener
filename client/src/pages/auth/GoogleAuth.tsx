0b8b2143213fcaf47323cf893f283384923edeaa
ronrix <ronrixl5@gmail.com>
2023-01-23 00:03:47 +0800

    Adding the reroute after successful login with google auth. Straight to dashboard

--- client/src/pages/auth/GoogleAuth.tsx
+++ client/src/pages/auth/GoogleAuth.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { GoogleLogin } from "@react-oauth/google";
 import jwtDecode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   handleErrMsg: () => void;
 };
 
 export default function GoogleAuth({ handleErrMsg }: Props) {
+  const navigate = useNavigate();
   return (
     <div className="mx-auto mb-5">
       <GoogleLogin
         onSuccess={async ({ credential }) => {
           const decoded: any = jwtDecode(credential as string);
 
-          await fetch("http://localhost:8000/google-auth", {
+          fetch("http://localhost:8000/google-auth", {
             method: "POST",
             body: JSON.stringify({
               id: decoded.sub,
@@ -25,7 +27,19 @@ export default function GoogleAuth({ handleErrMsg }: Props) {
               "Content-Type": "application/json",
             },
             credentials: "include",
-          });
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              if (data.status === 200) {
+                navigate("/dashboard");
+              } else {
+                handleErrMsg();
+              }
+            })
+            .catch((err) => {
+              console.log(err);
+              handleErrMsg();
+            });
         }}
         onError={handleErrMsg}
       />
