fbf4916994b4bba702adff720f7acd9ff9e31db4
ronrix <ronrixl5@gmail.com>
2023-01-23 13:29:00 +0800

    Add middleware for auth protection
    Validating the cookie token received from the client and responding 403 when it's not valida

---
+++ server/modules/middlewares/auth.middleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+  const token = req.cookies.token;
+
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, "SECRET", (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  })
+}
+
+module.exports = authenticateToken;
