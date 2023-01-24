76a5625ffa6ff1947d4901e65491b3e2d73733bd
ronrix <ronrixl5@gmail.com>
2023-01-24 23:43:03 +0800

    Added generate short url feature
    Adding the fetch to generete short url and error handler style if something went wrong when generating, and putting type to the btns to prevent from submiting the form

--- client/src/components/modals/AddCollection.tsx
+++ client/src/components/modals/AddCollection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
 type Props = {
@@ -9,6 +9,8 @@ export default function AddCollection({
   handleToggleAddCollectionModal,
 }: Props) {
   const { register, handleSubmit } = useForm();
+  const [generatedUrl, setGeneratedUrl] = useState<string>("");
+  const [err, setErr] = useState<string>("");
 
   function onSubmit() {
     fetch("http://localhost:8000/save-collection", {
@@ -30,12 +32,17 @@ export default function AddCollection({
       credentials: "include",
     })
       .then((res) => res.json())
-      .then(({ generatedUrl, status }) => {
-        if (status === 200) {
-          console.log(generatedUrl);
+      .then((data) => {
+        if (data.status === 200) {
+          setGeneratedUrl(data.generatedUrl);
+        } else {
+          setErr(data.msg);
         }
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setErr(err);
+      });
     console.log("generating...");
   }
 
@@ -58,28 +65,35 @@ export default function AddCollection({
             className="rounded w-full p-2 text-primary-black block outline-none"
           />
         </label>
-        <label className="text-white my-2 w-full">
+        {/* <label className="text-white my-2 w-full">
           Short URL <span className="text-grayish text-xs">(optional)</span>
           <p className="text-grays mb-2 text-xs">Add your own short url</p>
           <input
             {...register("prefix", { required: true })}
             className="rounded w-full p-2 text-primary-black block outline-none"
-          />
-        </label>
+          /> 
+        </label>*/}
         <div className="w-full mt-3">
           <button
+            type="button"
             onClick={generateShortUrl}
             className="bg-light-gray p-2 rounded"
           >
             Generate
           </button>
-          <span className="text-grayish text-sm ml-4">Random short url</span>
+          <span className="text-grays text-xs ml-2">Random short url</span>
           <p className="bg-tersiary-black text-grayish w-full p-2 text-sm mt-2 rounded">
-            https://goshort.com/dump
+            https://goshort.com/{generatedUrl || "dump"}
           </p>
         </div>
+        {err && (
+          <p className="bg-red-500 text-sm text-grayish w-full text-center px-2 mt-2 roundd-sm">
+            {err}
+          </p>
+        )}
         <div className="text-white self-end mt-5">
           <button
+            type="button"
             onClick={handleToggleAddCollectionModal}
             className="text-sm text-grayish cursor-pointer"
           >
