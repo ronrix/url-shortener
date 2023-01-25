79adfe55f290196e975f249459aaa30262817af0
ronrix <ronrixl5@gmail.com>
2023-01-26 01:24:23 +0800

    Adding saving collection feature
    Implementing the add function, when the user add new collection it is going to check if the user already has a collection, if it is. it's going to update the table of the user and append the new collection to url_collections column

--- server/controllers/dashboard.controller.js
+++ server/controllers/dashboard.controller.js
@@ -49,11 +49,14 @@ class DashboardController {
         const domain_name = url.hostname;
         const pathname = path.join(__dirname, "../", `images/${user.username}/`);
 
+        // this img_url will be stored in DB
+        const img_url = `/images/${user.username}/domain_name`;
+
         // genereate given website screenshot
         generateScreenshot(fields.originalUrl, pathname, domain_name)
         .then(() => {
             
-            this.dashboard.saveCollection(fields, pathname, user)
+            this.dashboard.saveCollection(fields, img_url+ domain_name, user)
             .then(data => {
                 console.log(data);
                 res.status(200).json({ msg: "Successlly saved the collection", status: 200 });
--- server/models/dashboard.model.js
+++ server/models/dashboard.model.js
@@ -1,8 +1,10 @@
 const Connection = require("../connection/mysql.connection");
+const FormValidation = require("../modules/validation/Validation");
 
 class DashboardModel {
     constructor() {
         this.conn = Connection;
+        this.formValidation = new FormValidation();
     }
 
     getUserInfo(user) {
@@ -26,6 +28,51 @@ class DashboardModel {
                 resolve(result[0]);
             });
         })
+    }
+
+    saveCollection(fields, img_url, user) {
+        console.log(user);
+
+        if(this.formValidation.is_empty(fields)) {
+            throw { msg: "Please fill up all the fields" ,status: 404 };
+        }
+
+        return new Promise(async (resolve, reject) => {
+            // check if the user already has a collection table
+            const collection_found = await this.conn._exists("SELECT * FROM collections WHERE user_id = ?", [user.id]);
+            if(collection_found) {
+                // update the url_collections column 
+                await this.conn.connection.execute(`
+                    UPDATE collections c
+                    JOIN (
+                        SELECT JSON_LENGTH(JSON_EXTRACT(url_collections, "$")) AS max_id
+                        FROM collections 
+                        WHERE user_id = ?
+                    ) t
+                    ON c.user_id = ?
+                    SET c.url_collections = JSON_ARRAY_APPEND(c.url_collections, '$',
+                        JSON_OBJECT(
+                            'id', max_id + 1, 
+                            'name', ?,
+                            'details', ?,
+                            'img_url', ?,
+                            'original_url', ?,
+                            'short_url', ?,
+                            'created_at', ?,
+                            'updated_at', ?))
+                    `, [user.id, user.id, fields.name, fields.details, img_url, fields.originalUrl, fields.shortUrl, new Date(), null], (err, result, _) => {
+                        if(err) {
+                            reject({msg: err, status: 500});
+                        }
+
+                        console.log(result);
+                    });
+                return;
+            }
+
+            // insert new collection if user has no collections yet
+
+        });
     }
 }
 
