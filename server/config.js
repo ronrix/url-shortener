de5bd7ade65143d1be8737c18aee1ab04bbc2651
ronrix <ronrixl5@gmail.com>
2023-01-26 16:28:12 +0800

    Removing extra / in base_url

--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
+const base_url = "http://localhost:8000";
 const PORT = 8000;
 
 const db_config = {
@@ -11,6 +12,7 @@ const db_config = {
 module.exports = {
 	PORT,
 	db_config,
+	base_url
 }
 
 /*
