9e6224187fe7e01efefa7dcc6ac58758d9fa9cac
ronrix <ronrixl5@gmail.com>
2023-01-27 22:24:06 +0800

    Removing unnecessary code

--- client/src/api/signout.ts
+++ client/src/api/signout.ts
@@ -1,12 +1,11 @@
  export async function signout(navigate: any) {
- fetch("http://localhost:8000/signout", {
+  fetch("http://localhost:8000/signout", {
       method: "GET",
       credentials: "include",
     })
       .then((res) => res.json())
       .then(({ msg, status }) => {
         // navigate to login page
-        console.log(msg);
         if (status === 200) {
           navigate("/login");
         }
