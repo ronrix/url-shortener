10c25cdcc39227f914d77dc6e744aa9d2f13d572
ronrix <ronrixl5@gmail.com>
2023-01-29 01:00:31 +0800

    Chore: Adding useForm
    Adding the yup validation schema with useForm

--- client/src/pages/dashboard/settings/account/InfoInput.tsx
+++ client/src/pages/dashboard/settings/account/InfoInput.tsx
@@ -1,16 +1,27 @@
 import React from "react";
+import { FieldValues } from "react-hook-form/dist/types/fields";
+import { UseFormRegister } from "react-hook-form/dist/types/form";
 
 type Props = {
   labelName: string;
   inputValue: string;
   note: string;
+  register: UseFormRegister<FieldValues>;
+  options: {};
 };
 
-export default function InfoInput({ labelName, inputValue, note }: Props) {
+export default function InfoInput({
+  labelName,
+  inputValue,
+  note,
+  register,
+  options,
+}: Props) {
   return (
     <label className="text-grayish font-bold block mt-2">
       {labelName}
       <input
+        {...register(labelName.toLowerCase())}
         type="text"
         value={inputValue}
         className="px-2 py-1 bg-transparent border border-grays outline-none focus:border-light-green rounded-md text-grayish block w-full font-thin"
--- client/src/pages/dashboard/settings/account/UserTable.tsx
+++ client/src/pages/dashboard/settings/account/UserTable.tsx
@@ -1,4 +1,8 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { updateProfileSchema } from "../../../../config/schema";
+
 import { CollectionContext, ContextType } from "../../../../context/collection";
 import Avatar from "./Avatar";
 import InfoInput from "./InfoInput";
@@ -7,34 +11,53 @@ export default function UserTable() {
   // get user data to display into DOM
   const { user } = useContext<ContextType>(CollectionContext);
 
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(updateProfileSchema),
+  });
+
+  function onSubmit(data: any) {
+    console.log(data);
+  }
+
   function handleShowPasswordModal() {
     alert("showing modal");
   }
 
   return (
-    <div className="flex justify-start align-center">
-      <div className="w-1/2">
-        <InfoInput
-          labelName="Username"
-          inputValue={user.username}
-          note="You can change your usernam but make sure that this is unique"
-        />
-        <InfoInput
-          labelName="Email"
-          inputValue={user.email}
-          note="By changing your email, you have to make sure that your email is
+    <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+      <div className="flex justify-start align-center">
+        <div className="w-1/2">
+          <InfoInput
+            register={register}
+            labelName="Username"
+            inputValue={user.username}
+            options={{ required: true, minLength: 8 }}
+            note="You can change your usernam but make sure that this is unique"
+          />
+          <InfoInput
+            register={register}
+            labelName="Email"
+            inputValue={user.email}
+            options={{ required: true, email: true }}
+            note="By changing your email, you have to make sure that your email is
             verified. This email will be your account security when your
             password has been compromised."
-        />
-        <button
-          type="button"
-          className="text-light-green mt-3"
-          onClick={() => handleShowPasswordModal()}
-        >
-          Change password
-        </button>
+          />
+          <button
+            type="button"
+            className="text-light-green mt-3"
+            onClick={() => handleShowPasswordModal()}
+          >
+            Change password
+          </button>
+        </div>
+        <Avatar />
       </div>
-      <Avatar />
-    </div>
+      <input
+        type="submit"
+        value="Update profile"
+        className="mt-5 px-4 py-1 bg-dark-green rounded-md font-thin cursor-pointer hover:bg-light-green text-grayish"
+      />
+    </form>
   );
 }
