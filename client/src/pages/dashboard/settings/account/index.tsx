40d9f181f13ebe496caba6d3aa4f5c93dce3a207
ronrix <ronrixl5@gmail.com>
2023-01-29 00:58:20 +0800

    Remove: removing states and styles

--- client/src/pages/dashboard/settings/account/index.tsx
+++ client/src/pages/dashboard/settings/account/index.tsx
@@ -1,21 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import UserTable from "./UserTable";
 
 export default function Account() {
-  const [username, setUsername] = useState<{
-    editable: boolean;
-    value: string;
-  }>({ editable: false, value: "" });
-  const [password, setPassword] = useState<{
-    editable: boolean;
-    value: string;
-  }>({ editable: false, value: "" });
-
   return (
     <div className="">
       <h4 className="text-grayish font-bold">Account Settings</h4>
       <p className="text-grays text-sm">You personal account</p>
-      <div className="mt-3 text-grayish flex justify-between items-center">
+      <div className="mt-3">
         <UserTable />
       </div>
     </div>
