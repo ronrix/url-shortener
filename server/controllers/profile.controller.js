d308d7e984a253c91f32049ef9645ab057b093b1
ronrix <ronrixl5@gmail.com>
2023-01-30 01:22:24 +0800

    Feat: update password backend functionality

--- server/controllers/profile.controller.js
+++ server/controllers/profile.controller.js
@@ -17,6 +17,19 @@ class ProfileController {
             res.status(err.status).json(err);
         });
     }
+
+    updatePassword = (req, res) => {
+        const user = req.user;
+        const fields = req.body;
+
+        this.profile.updatePassword(user, fields) .then(response => {
+            console.log(response);
+            res.status(200).json(response);
+        }).catch(err => {
+            console.log(err)
+            res.status(err.status).json(err);
+        })
+    }
 }
 
 module.exports = new ProfileController();
--- server/routes/profile.route.js
+++ server/routes/profile.route.js
@@ -5,5 +5,6 @@ const ProfileController = require("../controllers/profile.controller");
 const authenthicateToken = require("../modules/middlewares/auth.middleware");
 
 router.post("/update-profile", [authenthicateToken], ProfileController.edit);
+router.post("/update-password", [authenthicateToken], ProfileController.updatePassword);
 
 module.exports = router;
