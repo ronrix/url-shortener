149003dba171daed42f309c8232c8f0d44838809
ronrix <ronrixl5@gmail.com>
2023-01-30 00:00:24 +0800

    Chore: adding notification message after a fetch request

--- client/src/pages/dashboard/settings/account/UserTable.tsx
+++ client/src/pages/dashboard/settings/account/UserTable.tsx
@@ -1,6 +1,7 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
+import Notif from "../../../../components/modals/Notif";
 import { updateProfileSchema } from "../../../../config/schema";
 
 import { CollectionContext, ContextType } from "../../../../context/collection";
@@ -10,6 +11,8 @@ import InfoInput from "./InfoInput";
 export default function UserTable() {
   // get user data to display into DOM
   const { user } = useContext<ContextType>(CollectionContext);
+  const [showNotif, setShowNotif] = useState<boolean>(false);
+  const [resMsg, setResMsg] = useState<{ msg: string; status: number }>();
 
   const { register, handleSubmit } = useForm({
     resolver: yupResolver(updateProfileSchema),
@@ -29,6 +32,12 @@ export default function UserTable() {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        if (data.status === 200) {
+          setShowNotif(true);
+          setResMsg(data);
+
+          setTimeout(() => setShowNotif(false), 2000);
+        }
       })
       .catch((err) => console.log(err));
   }
@@ -38,38 +47,52 @@ export default function UserTable() {
   }
 
   return (
-    <form onSubmit={handleSubmit((data) => onSubmit(data))}>
-      <div className="flex justify-start align-center">
-        <div className="w-1/2">
-          <InfoInput
-            register={register}
-            labelName="Username"
-            inputValue={user.username}
-            note="You can change your usernam but make sure that this is unique"
-          />
-          <InfoInput
-            register={register}
-            labelName="Email"
-            inputValue={user.email}
-            note="By changing your email, you have to make sure that your email is
+    <>
+      {showNotif && <Notif resMsg={resMsg} />}
+      <form onSubmit={handleSubmit((data) => onSubmit(data))}>
+        <div className="flex justify-start align-center">
+          <div className="w-1/2">
+            <InfoInput
+              register={register}
+              labelName="Username"
+              inputValue={user.username}
+              note="You can change your usernam but make sure that this is unique"
+            />
+            <InfoInput
+              register={register}
+              labelName="Email"
+              inputValue={user.email}
+              note="By changing your email, you have to make sure that your email is
             verified. This email will be your account security when your
             password has been compromised."
-          />
-          <button
-            type="button"
-            className="text-light-green mt-3"
-            onClick={() => handleShowPasswordModal()}
-          >
-            Change password
-          </button>
+            />
+            <button
+              type="button"
+              className="text-light-green mt-3"
+              onClick={() => handleShowPasswordModal()}
+            >
+              Change password
+            </button>
+          </div>
+          <Avatar />
         </div>
-        <Avatar />
-      </div>
-      <input
-        type="submit"
-        value="Update profile"
-        className="mt-5 px-4 py-1 bg-dark-green rounded-md font-thin cursor-pointer hover:bg-light-green text-grayish"
-      />
-    </form>
+
+        <input
+          type="submit"
+          value="Update profile"
+          className="mt-5 px-4 py-1 bg-dark-green rounded-md font-thin cursor-pointer hover:bg-light-green text-grayish"
+        />
+        <p className="text-xs text-grays mt-2">
+          You won't see the changes until you{" "}
+          <span
+            className="text-grayish cursor-pointer"
+            onClick={() => window.location.reload()}
+          >
+            reload
+          </span>{" "}
+          the page
+        </p>
+      </form>
+    </>
   );
 }
