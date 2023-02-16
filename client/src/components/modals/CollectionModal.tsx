7574332750d99431e2d9d198f9b8e1516dae1cca
ronrix <medyochain@gmail.com>
2023-02-16 18:33:35 +0800

    feature
    adding udpate feature for collection and added a notification message. I also added loading animation on save button and a small css style

--- client/src/components/modals/CollectionModal.tsx
+++ client/src/components/modals/CollectionModal.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { URL_COLLECTIONS_TYPE } from "../../context/collection";
+import Notif from "./Notif";
 
 type Props = {
   handleShowModal: () => void;
@@ -12,11 +13,44 @@ export default function CollectionModal({
   collection,
   img_url,
 }: Props) {
+  const [edit, setEdit] = useState<{collection_name: boolean; collection_details: boolean; original_url: boolean}>({ collection_name: false, collection_details: false, original_url: false });
+  const [collectionInputs, setCollectionInputs] = useState<{collection_name: string; collection_details: string; original_url: string}>({ collection_name: collection.name, collection_details: collection.details, original_url: collection.original_url });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [showNotif, setShowNotif] = useState<boolean>(false);
+  const [resMsg, setResMsg] = useState<{msg: string; status: number}>();
+
+  const [isUpdatedCollections, setUpdatedCollections] = useState<boolean>(() => !(collection.details.trim() === collectionInputs.collection_details.trim() && collection.name === collectionInputs.collection_name.trim() && collection.original_url.trim() === collectionInputs.original_url.trim()));
+
   function handleCancelEditedCollection() {
-    console.log("cancelling...");
+    // revert back to orginal values
+    setCollectionInputs({collection_name: collection.name, collection_details: collection.details, original_url: collection.original_url});
+    setUpdatedCollections(false);
   }
-  function handleSaveCollection() {
-    console.log("saving...");
+
+  async function handleSaveCollection() {
+    // TODO: send updated collection to the server and update it
+    setIsLoading(true);
+    try {
+      const response = await fetch(import.meta.env.VITE_BACKEND_URL + "edit-collection", { method: "POST", credentials: "include", body: JSON.stringify({ ...collectionInputs, id: collection.id }), headers: { "Content-Type": "application/json"} });
+      const data = await response.json();
+
+      setResMsg(data);
+      setShowNotif(true);
+      setUpdatedCollections(false);
+      setIsLoading(false);
+      
+      // update the state collection value with the new one
+      collection.details = collectionInputs.collection_details;
+      collection.name = collectionInputs.collection_name;
+      collection.original_url = collectionInputs.original_url;
+
+      setTimeout(() => {
+        setShowNotif(false);
+      }, 2000);
+
+    } catch(err) {
+      console.log(err);
+    }
   }
 
   async function handleRedirectToUrl() {
@@ -32,11 +66,29 @@ export default function CollectionModal({
     }
   }
 
+  function makeEditable(e: React.MouseEvent<HTMLHeadingElement | HTMLParagraphElement>) {
+    setEdit({ ...edit, [e.currentTarget.id]: true });
+  } 
+
+  function handleBlur(e: React.FocusEvent<HTMLTextAreaElement, Element>) {
+    setEdit({ ...edit, [e.target.name]: false });
+  }
+
+  function handleInputs(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setCollectionInputs(prev => ({ ...prev, [e.target.name]: e.target.value }));
+  }
+
+  // this checks if the inputs changes value so we can display the buttons
+  useEffect(() => {
+    setUpdatedCollections(() => !(collection.details.trim() === collectionInputs.collection_details.trim() && collection.name.trim() === collectionInputs.collection_name.trim() && collection.original_url.trim() === collectionInputs.original_url.trim()));
+  }, [isUpdatedCollections, collectionInputs]);
+
   return (
     <div
       className="fixed top-0 left-0 right-0 bottom-0 bg-primary-black bg-opacity-60 flex items-center justify-center"
       onClick={handleShowModal}
     >
+      {showNotif && <Notif resMsg={resMsg} />}
       <div
         className="w-full md:w-700 rounded-lg bg-secondary-black text-white p-5 flex flex-col md:flex-row relative"
         onClick={(e) => {
@@ -54,13 +106,21 @@ export default function CollectionModal({
         </div>
 
         <div className="flex-1 break-words overflow-x-hidden mt-5 md:mt-0">
-          <h3 className="text-grayish font-bold text-lg uppercase">{collection.name}</h3>
-          <p className="text-grays">{collection.details}</p>
+          
+          { edit.collection_name ?  (
+            <textarea autoFocus name="collection_name" onChange={handleInputs} onBlur={handleBlur} className="font-bold text-lg uppercase w-full bg-transparent mb-2 text-grayish border border-grays outline-none max-h-[100px] min-h-[80px]" defaultValue={collectionInputs.collection_name}></textarea> ) : (
+            <h3 id="collection_name" onClick={makeEditable} className="text-grayish font-bold text-lg uppercase">{collectionInputs.collection_name}</h3> )
+          }
+          {edit.collection_details ? (
+            <textarea autoFocus name="collection_details" onChange={handleInputs} onBlur={handleBlur} className="text-lg w-full bg-transparent mb-2 text-grayish border border-grays outline-none max-h-[100px] min-h-[80px]" defaultValue={collectionInputs.collection_details}></textarea> ) : (
+            <p id="collection_details" className="text-grays" onClick={makeEditable}>{collectionInputs.collection_details}</p> )
+          }
 
           <h4 className="text-grayish mt-3">Original URL</h4>
-          <p className="text-grays text-xs underline text-justify w-full">
-            {collection.original_url}
-          </p>
+          { edit.original_url ? (
+            <textarea autoFocus name="original_url" onChange={handleInputs} onBlur={handleBlur} className="text-lg w-full bg-transparent mb-2 text-grayish border border-grays outline-none max-h-[100px] min-h-[40px]" defaultValue={collectionInputs.original_url}></textarea> ) : (
+            <p id="original_url" onClick={makeEditable} className="text-grays text-xs underline text-justify w-full">{collectionInputs.original_url}</p>
+          )}
 
           <h4 className="text-grayish mt-3">Short URL</h4>
           <div className="flex justify-between text-xs">
@@ -73,20 +133,32 @@ export default function CollectionModal({
             </div>
           </div>
 
-          <div className="flex items-center justify-end mt-5">
-            <button
-              className="px-3 py-1 text-grayish"
-              onClick={handleCancelEditedCollection}
-            >
-              Cancel
-            </button>
-            <button
-              className="px-3 py-1 text-grayish bg-dark-green rounded-lg"
-              onClick={handleSaveCollection}
-            >
-              Save
-            </button>
-          </div>
+          {isUpdatedCollections ? (
+            <div className="flex items-center justify-end mt-5">
+              <button
+                className="px-3 py-1 text-grayish"
+                onClick={handleCancelEditedCollection}
+              >
+                Cancel
+              </button>
+              <button
+                className={`px-3 py-1 text-grayish rounded-lg flex ${isLoading ? "bg-grays" : "bg-dark-green "}`}
+                onClick={handleSaveCollection}
+                disabled={isLoading}
+              >
+              {isLoading && (
+                <img
+                  src={"../../src/assets/loading.gif"}
+                  alt="loading animation"
+                  className="w-5 mr-2"
+                />
+              )}
+                Save
+              </button>
+            </div>
+          ) : null}
+
+          <p className="text-xs text-grays text-center mt-5">Note: clicking the text will let you edit it</p>
         </div>
       </div>
     </div>
